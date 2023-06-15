3e75188ceacb4320fb99395b90aee0b8c254affe
Abishek-project <abishekabi992@gmail.com>
2023-06-15 21:32:04 +0530

    first commit

---
+++ android/app/src/main/java/com/example/shoe_shop_ui/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.shoe_shop_ui;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/screens/intro_page_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class IntroPageView extends StatelessWidget {
+  const IntroPageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
